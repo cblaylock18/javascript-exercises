d06f3dd938f59f3f1cac67e7a761097bc9c896a1
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-03-27 18:49:55 -0400

    Complete exercise two and all associated tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (forwardString) {
+    let reversedString = "";
+    for (let i = forwardString.length; i >= 0; i--) {
+        reversedString += forwardString.slice(i - 1, i);
+    }
 
+    return reversedString;
 };
 
 // Do not edit below this line

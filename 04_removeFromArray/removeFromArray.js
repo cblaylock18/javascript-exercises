6c7cd30d1ebaeefabfe55cc982a684b6714a337f
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-03-27 20:01:55 -0400

    Complete exercise 4 and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (userArray, ...filterValues) {
+    let i = userArray.length - 1;
+    while (i >= 0) {
+        for (let j = 0; j < filterValues.length; j++) {
+            if (userArray[i] === filterValues[j]) {
+                userArray.splice(i, 1);
+            }
+        }
+        i--;
+    }
+    return userArray;
 };
 
 // Do not edit below this line

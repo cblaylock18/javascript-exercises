561cb4c5c466ba53341f6d740a6bcd8b9de9c636
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-03-28 20:05:36 -0400

    Complete exercise 5 and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+    let smallNumber =
+        firstNumber < 0 ||
+        typeof firstNumber !== "number" ||
+        typeof secondNumber !== "number"
+            ? "ERROR"
+            : firstNumber <= secondNumber
+            ? firstNumber
+            : firstNumber > secondNumber
+            ? secondNumber
+            : "ERROR";
+    if (smallNumber === "ERROR") {
+        return smallNumber;
+    }
+    let largeNumber = smallNumber === firstNumber ? secondNumber : firstNumber;
+    let numberSum = 0;
+    while (smallNumber <= largeNumber) {
+        numberSum += smallNumber;
 
+        smallNumber++;
+    }
+    return numberSum;
 };
 
 // Do not edit below this line

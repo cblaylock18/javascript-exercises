fc9c3e5fcfcf202fd61e1712bc69791e11f14514
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-04-01 19:35:33 -0400

    Complete exercise 7 and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,6 +1,20 @@
-const convertToCelsius = function () {};
+const convertToCelsius = function (fahrenheitTemperature) {
+    celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+    if (celsiusTemperature === Math.round(celsiusTemperature)) {
+        return celsiusTemperature;
+    } else {
+        return celsiusTemperature.toFixed(1) * 1;
+    }
+};
 
-const convertToFahrenheit = function () {};
+const convertToFahrenheit = function (celsiusTemperature) {
+    fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    if (fahrenheitTemperature === Math.round(fahrenheitTemperature)) {
+        return fahrenheitTemperature;
+    } else {
+        return fahrenheitTemperature.toFixed(1) * 1;
+    }
+};
 
 // Do not edit below this line
 module.exports = {

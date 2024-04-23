1f444eb0e26c7ced10d056e7c0fd677b78e4c705
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-04-22 20:13:22 -0400

    Complete exercise 08 and pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,51 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+    let total = numbers.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+    );
+    return total;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+    return numbers.reduce(
+        (accumulator, currentValue) => accumulator * currentValue,
+        1
+    );
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    let currentValue = 1;
+    for (let i = 1; i <= b; i++) {
+        currentValue *= a;
+    }
+    return currentValue;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    let currentValue = 1;
+    let i = 1;
+    do {
+        currentValue *= i;
+        i++;
+        console.log(currentValue);
+    } while (i <= a);
+    return currentValue;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };

7833f36a3fc807743fd542e1b509012640595549
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-04-22 20:13:22 -0400

    Complete exercise 11 and pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    people.forEach((person) => {
+        if (person.hasOwnProperty("yearOfDeath")) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = new Date().getFullYear() - person.yearOfBirth;
+        }
+    });
+    people.sort((a, b) => b.age - a.age);
+    return people[0];
 };
 
 // Do not edit below this line

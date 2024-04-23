8c16da37ed48257a0240d9a2a3fce9e9b32b2e76
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-04-22 20:13:22 -0400

    Complete exercise 10 and pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    fibonacciSequence = [0, 1, 1];
+    if (number <= 2 && number >= 0) {
+        return fibonacciSequence[number];
+    } else if (number > 2) {
+        for (let i = 2; i <= number; i++) {
+            fibonacciSequence.push(
+                fibonacciSequence[i] + fibonacciSequence[i - 1]
+            );
+        }
+        return fibonacciSequence[number];
+    } else return "OOPS";
 };
 
 // Do not edit below this line

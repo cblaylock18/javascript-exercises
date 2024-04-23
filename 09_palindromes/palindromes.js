d6de907dbd0be978be7e0ec62186c6b40d3071cc
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-04-22 20:13:22 -0400

    Complete exercise 09 and pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let reverseString = "";
+    let rawString = string
+        .toUpperCase()
+        .replace(/[^\p{L}\p{N}\s]/gu, "")
+        .replaceAll(" ", "");
 
+    for (let i = 0; i < rawString.length; i++) {
+        reverseString += rawString.slice(
+            rawString.length - i - 1,
+            rawString.length - i
+        );
+    }
+    console.log(rawString);
+    console.log(reverseString);
+    return rawString === reverseString;
 };
 
 // Do not edit below this line
